f9b9467328a64d6b9325c6d1734f0e19a8f39aad
Kevin Roan <kevinroan0303@gmail.com>
2023-09-23 23:31:05 +0530

    material-tailwind boilerplate

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Button } from "@material-tailwind/react";
+import React from "react";
+import "./App.css";
+import LayoutRoutes from "./RouteLayouts/LayoutRoutes";
+
+function App() {
+  return <LayoutRoutes />;
+}
+
+export default App;
---
+++ src/RouteLayouts/LayoutRoutes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavbarDefault } from "../components/NavBar";
+
+const LayoutRoutes = () => {
+  return (
+    <div>
+      <NavbarDefault />
+    </div>
+  );
+};
+
+export default LayoutRoutes;
