a075bbcf9146c31aae540c56cec594e9541f016d
Kevin Roan <kevinroan0303@gmail.com>
2023-10-02 22:30:11 +0530

    responsive about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,49 +1,41 @@
-import { Typography } from "@material-tailwind/react";
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import "./About.css";
 
 const About = () => {
   return (
     <>
-      <CenterDiv />
+      <div className="grid place-content-center mt-9 greeting-container">
+        <h1>Welcome to my</h1>
+        <TextTyper />
+      </div>
     </>
   );
 };
 
-export default About;
-
-const ExampleComponent = () => {
+const TextTyper = () => {
   return (
     <>
       <TypeAnimation
         sequence={[
-          "Portfolio", // Types 'One'
-          2000, // Waits 1s
-          "Hobby", // Deletes 'One' and types 'Two'
-          2000, // Waits 2s
-          "Passion", // Types 'Three' without deleting 'Two'
-          2000, // Waits 2s
-          "Project", // Types 'Three' without deleting 'Two'
-          2000, // Waits 2s
-          "Playground", // Types 'Three' without deleting 'Two'
-          2000, //
+          "Portfolio",
+          2000,
+          "Hobby",
+          2000,
+          "Passion",
+          2000,
+          "Project",
+          2000,
+          "Playground",
+          2000,
         ]}
         wrapper="span"
         cursor={true}
         repeat={Infinity}
-        style={{ fontSize: "30px", display: "inline-block", color: "#fff" }}
+        className="text-typer"
       />
     </>
   );
 };
 
-const CenterDiv = () => {
-  return (
-    <div className="grid place-content-center mt-9 ">
-      <h1 style={{ color: "#fff", fontSize: "5rem", fontFamily: "sans-serif" }}>
-        Welcome to my
-      </h1>
-      <ExampleComponent />
-    </div>
-  );
-};
+export default About;
