66de6aa87fb838c2622b4652f4d53f659bf8499e
Kevin Roan <kevinroan0303@gmail.com>
2023-10-07 18:35:06 +0530

    pre-clean up

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { Carousel, Typography } from "@material-tailwind/react";
+import { Button, Carousel, Typography } from "@material-tailwind/react";
 import project from "../assets/computer.svg";
 
 export function CarouselCustomNavigation() {
@@ -33,14 +33,15 @@ export function CarouselCustomNavigation() {
       <figcaption className="absolute bottom-8 left-2/4 flex w-[calc(100%-4rem)] -translate-x-2/4 justify-between rounded-xl border border-white bg-white/75 py-4 px-6 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm">
         <div>
           <Typography variant="h5" color="blue-gray">
-            Sara Lamalo
+            RestHUB - React Restorant Website
           </Typography>
           <Typography color="gray" className="mt-2 font-normal">
             20 July 2022
           </Typography>
         </div>
         <Typography variant="h5" color="blue-gray">
-          Growth
+          <Button style={{ margin: "10px" }}>Demo</Button>
+          <Button>Source</Button>
         </Typography>
       </figcaption>
     </Carousel>
